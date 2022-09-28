149ef8fc1fa5ed24a1774abbc12cbe5b7a353d74
shailaksy <shailaksy@gmail.com>
2022-09-28 23:47:25 +0800

    added designs to admin page

--- src/components/Bank/Functions.js
+++ src/components/Bank/Functions.js
@@ -1,14 +1,19 @@
 import './Functions.css';
+import deposit from "../../assets/deposit.png";
+import withdraw from "../../assets/withdraw.png";
+import transfer from "../../assets/transfer.png";
+import balance from "../../assets/balance.png";
+import users from "../../assets/users.png";
 
 function Functions() {
     return(
         <div className="functions">
             <ul>
-                <li><button><i className="uil-money-insert"></i></button><p>Deposit</p></li>
-                <li><button><i className="uil-money-withdraw"></i></button><p>Withdraw</p></li>
-                <li><button><i className="uil-exchange"></i></button><p>Transfer</p></li>
-                <li><button><i className="uil-bill"></i></button><p>Balance Inquiry</p></li>
-                <li><button><i className="uil-users-alt"></i></button><p>Users Directory</p></li>
+                <li><button><img src={deposit} className="deposit"></img></button><p>Deposit</p></li>
+                <li><button><img src={withdraw} className="withdraw"></img></button><p>Withdraw</p></li>
+                <li><button><img src={transfer} className="transfer"></img></button><p>Transfer</p></li>
+                <li><button><img src={balance} className="balance"></img></button><p>Balance Inquiry</p></li>
+                <li><button><img src={users} className="users"></img></button><p>Users Directory</p></li>
             </ul>
         </div>
     )
