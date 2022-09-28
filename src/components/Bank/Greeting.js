39e217397c0cc7bfdaa62fbc5ebbef2d29edfab1
shailaksy <shailaksy@gmail.com>
2022-09-28 19:27:18 +0800

    added to admin page

---
+++ src/components/Bank/Greeting.js
@@ -0,0 +1,12 @@
+import './Greeting.css';
+
+function Greeting() {
+    return(
+        <div className="greeting">
+            <h3>September 27, 2022</h3>
+            <h1>Hello, User</h1>
+        </div>
+    )
+}
+
+export default Greeting;
