fc49b0412ad71828bab82d697d3bcb7909935dc7
shailaksy <shailaksy@gmail.com>
2022-09-29 20:47:21 +0800

    updated designs based on feedback

--- src/components/Bank/Functions.js
+++ src/components/Bank/Functions.js
@@ -9,11 +9,11 @@ function Functions() {
     return(
         <div className="functions">
             <ul>
-                <li><button><img src={deposit} className="deposit"></img></button><p>Deposit</p></li>
-                <li><button><img src={withdraw} className="withdraw"></img></button><p>Withdraw</p></li>
-                <li><button><img src={transfer} className="transfer"></img></button><p>Transfer</p></li>
-                <li><button><img src={balance} className="balance"></img></button><p>Balance Inquiry</p></li>
-                <li><button><img src={users} className="users"></img></button><p>Users Directory</p></li>
+                <li><button><img src={deposit} className="deposit" alt='deposit'></img></button><p>Deposit</p></li>
+                <li><button><img src={withdraw} className="withdraw" alt='withdraw'></img></button><p>Withdraw</p></li>
+                <li><button><img src={transfer} className="transfer" alt='transfer'></img></button><p>Transfer</p></li>
+                <li><button><img src={balance} className="balance" alt='balance'></img></button><p>Balance Inquiry</p></li>
+                <li><button><img src={users} className="users" alt='users'></img></button><p>Users Directory</p></li>
             </ul>
         </div>
     )
